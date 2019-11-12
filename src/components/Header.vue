245981009aa197e5afb8fe56cc354fb1545b54a5
sakamoto <sakamoto@sakamotonoMacBook-Pro.local>
2019-11-12 10:42:17 +0900

    add header

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header id="header">
+    <h1>YUUTA SAKAMOTO</h1>
+  </header>
+</template>
+
+<style scoped>
+header {
+  height: 100px;
+  background-color: #42b983;
+  color: white;
+  position: relative;
+}
+
+h1 {
+  text-align: center;
+  top: 50%;
+  -webkit-transform: translateY(-50%);
+  transform: translateY(-50%);
+  position: absolute;
+}
+</style>
